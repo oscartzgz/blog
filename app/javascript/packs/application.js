db19c8f03b5c689810646aa7691349bd0c02d25a
Oscar Tellez <oscartzgz@gmail.com>
2019-10-25 13:09:06 -0500

    Add Bulma CSS framework :D

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import 'css/styles.scss'
+
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,3 +17,20 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
+
+document.addEventListener("turbolinks:load", function() {
+
+  new_article_form = document.getElementById('new_article_form')
+  edit_article_form = document.getElementById('edit_article_form')
+  
+  new_article_form.addEventListener("ajax:success", (event) => alert('Article was created.'))
+  edit_article_form.addEventListener("ajax:success", (event) => alert('Article was updated.'))
+
+  new_article_form.addEventListener("ajax:error", function(event) {
+    console.log(event)
+  })
+  edit_article_form.addEventListener("ajax:error", (event) => alert(event.details))
+})
+
